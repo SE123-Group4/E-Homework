3edbb1897f6ad5422d5c2fbc45c17ee9654a3784
Liao Yichen <echo.lyc@sjtu.edu.cn>
2020-11-20 17:48:31 +0800

    feat: add rich text

--- TechPrototype/frontend/Screens/AssignHwScreen.js
+++ TechPrototype/frontend/Screens/AssignHwScreen.js
@@ -11,7 +11,6 @@ import {
   Right,
   Header,
   Title,
-  Textarea,
   Form,
   Item,
   ListItem,
@@ -23,11 +22,10 @@ import {
   Switch,
   DatePicker,
   Picker,
-  Label,
 } from 'native-base';
-import {Overlay} from 'react-native-elements';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import {ScrollView, StyleSheet} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
+import {RichText} from '../Components/RichText';
 
 export class AssignHwScreen extends React.Component {
   static propTypes = {
@@ -96,143 +94,16 @@ export class AssignHwScreen extends React.Component {
   render() {
     return (
       <ScrollView>
-        <Overlay
-          isVisible={this.state.ifRichTextShow}
-          onBackdropPress={() => {
-            this.setState({
-              ifRichTextShow: false,
-              richText: {text: '', fileList: []},
-            });
-          }}>
-          <Container>
-            <Content
-              contentContainerStyle={{
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Textarea
-                rowSpan={8}
-                bordered
-                style={{fontSize: 18, color: 'black'}}
-                value={this.state.richText.text}
-                placeholder="请输入文本内容                                         "
-                onChangeText={(value) => {
-                  let richText = this.state.richText;
-                  richText.text = value;
-                  this.setState({richText: richText});
-                }}
-              />
-              <Card style={{width: '100%', marginTop: 10}}>
-                <CardItem bordered>
-                  <Content
-                    contentContainerStyle={{
-                      flexDirection: 'row',
-                      justifyContent: 'space-around',
-                    }}>
-                    <Button
-                      transparent
-                      style={{
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                      }}
-                      onPress={() => {
-                        const options = {
-                          quality: 1.0,
-                          maxWidth: 500,
-                          maxHeight: 500,
-                          storageOptions: {
-                            skipBackup: true,
-                          },
-                        };
-                      }}>
-                      <Icon
-                        name="camera"
-                        type="FontAwesome5"
-                        style={{color: '#0093fe'}}
-                      />
-                      <Text style={{fontSize: 13, color: '#0093fe'}}>
-                        添加图片
-                      </Text>
-                    </Button>
-                    <Button
-                      transparent
-                      style={{
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                      }}
-                      onPress={() => {}}>
-                      <Icon
-                        name="file"
-                        type="FontAwesome"
-                        style={{color: '#0093fe'}}
-                      />
-                      <Text style={{fontSize: 13, color: '#0093fe'}}>
-                        添加文件
-                      </Text>
-                    </Button>
-                    <Button
-                      transparent
-                      style={{
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                      }}
-                      onPress={() => {}}>
-                      <Icon
-                        name="microphone"
-                        type="FontAwesome"
-                        style={{color: '#0093fe'}}
-                      />
-                      <Text style={{fontSize: 13, color: '#0093fe'}}>
-                        语音识别
-                      </Text>
-                    </Button>
-                    <Button
-                      transparent
-                      style={{
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                      }}
-                      onPress={() => {}}>
-                      <Icon
-                        name="font"
-                        type="FontAwesome5"
-                        style={{color: '#0093fe'}}
-                      />
-                      <Text style={{fontSize: 13, color: '#0093fe'}}>
-                        文字识别
-                      </Text>
-                    </Button>
-                  </Content>
-                </CardItem>
-              </Card>
-              <Button
-                style={{
-                  marginTop: 10,
-                  alignSelf: 'center',
-                  backgroundColor: '#0093fe',
-                }}
-                onPress={() => {
-                  this.setState({
-                    ifRichTextShow: false,
-                    richText: {text: '', fileList: []},
-                  });
-                }}>
-                <Text
-                  style={{
-                    fontSize: 20,
-                    fontWeight: 'bold',
-                    color: 'white',
-                  }}>
-                  确 定
-                </Text>
-              </Button>
-            </Content>
-          </Container>
-        </Overlay>
+        <RichText
+          ifRichTextShow={this.state.ifRichTextShow}
+          richText={this.state.richText}
+          setShow={(value) => {
+            this.setState({ifRichTextShow: value});
+          }}
+          setRichText={(value) => {
+            this.setState({richText: value});
+          }}
+        />
         <Container style={{height: 'auto'}}>
           <Header
             style={{
