5b1fc99aab8365e0fd25fe4d9da3435ed9179ed4
Liao Yichen <echo.lyc@sjtu.edu.cn>
2021-01-06 20:42:48 +0800

    fix: assign hw screen

--- FinalRelease/frontend/Service/HomeworkService.js
+++ FinalRelease/frontend/Service/HomeworkService.js
@@ -1,7 +1,16 @@
 import {postRequest} from '../Util/Ajax';
+import {getRequest} from '../Util/Ajax';
 import {HOMEWORK_URL} from '../Constant/Url';
 
 export const getStuAnswer = (homeworkAssignID, callback) => {
   var data = {homeworkAssignID: homeworkAssignID};
   postRequest(HOMEWORK_URL + 'student_answer', data, callback);
+};
+
+export const getAssignHomework = (homeworkID, callback) => {
+  getRequest(HOMEWORK_URL + 'AssignHomework?hwId=' + homeworkID, callback);
+};
+
+export const postAssignHomework = (data, callback) => {
+  postRequest(HOMEWORK_URL + 'AssignHomework', data, callback);
 };
