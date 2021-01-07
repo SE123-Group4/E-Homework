3b4ce9e1163fc6fbf49901d8e59e5e10fc01b905
KingCross9-34 <2568930409@qq.com>
2021-01-07 09:13:25 +0800

    modify

--- FinalRelease/frontend/Components/HomeworkInfo.js
+++ FinalRelease/frontend/Components/HomeworkInfo.js
@@ -17,13 +17,13 @@ export class HomeworkInfo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      course: '这是班级名称',
+      title: '软件工程原理及实践大作业',
       teacher: '老师XXX',
-      start: '10-10 00:00:00',
-      deadline: '10-10 12:00:00',
-      content: '这是作业内容',
-      answer: '这是参考答案',
-      comment: '这是整体点评',
+      start: '2020-10-10 00:00:00',
+      deadline: '2020-10-10 12:00:00',
+      //content: '这是作业内容',
+      //answer: '这是参考答案',
+      //comment: '这是整体点评',
       maxScore: 98,
       minScore: 61,
       averageScore: 80,
@@ -48,7 +48,7 @@ export class HomeworkInfo extends React.Component {
       <Card>
         <CardItem>
           <Body>
-            <Text style={styles.Header}>{this.state.course}</Text>
+            <Text style={styles.Header}>{this.state.title}</Text>
             <Text style={styles.FootNoteGray}>
               {this.state.teacher}于{this.state.start}布置
             </Text>
@@ -57,30 +57,30 @@ export class HomeworkInfo extends React.Component {
             </Text>
           </Body>
         </CardItem>
-        <CardItem>
-          <Body>
-            <Text style={styles.Body}>{this.state.content}</Text>
-          </Body>
-        </CardItem>
-        <CardItem
-          button
-          onPress={() => {
-            console.log('答题卡');
-          }}>
-          <Left>
-            <Icon type="FontAwesome" name="book" style={{color: '#0093fe'}} />
-            <Body>
-              <Text style={styles.Body}>答题卡</Text>
-              <Text style={styles.FootNoteGray}>点击查看答题结果</Text>
-            </Body>
-          </Left>
-        </CardItem>
-        <CardItem bordered>
-          <Body>
-            <Text style={styles.FootNoteGray}>参考答案 提交后可见</Text>
-            <Text style={styles.Body}>{this.state.answer}</Text>
-          </Body>
-        </CardItem>
+        {/*<CardItem>*/}
+        {/*  <Body>*/}
+        {/*    <Text style={styles.Body}>{this.state.content}</Text>*/}
+        {/*  </Body>*/}
+        {/*</CardItem>*/}
+        {/*<CardItem*/}
+        {/*  button*/}
+        {/*  onPress={() => {*/}
+        {/*    console.log('答题卡');*/}
+        {/*  }}>*/}
+        {/*  <Left>*/}
+        {/*    <Icon type="FontAwesome" name="book" style={{color: '#0093fe'}} />*/}
+        {/*    <Body>*/}
+        {/*      <Text style={styles.Body}>答题卡</Text>*/}
+        {/*      <Text style={styles.FootNoteGray}>点击查看答题结果</Text>*/}
+        {/*    </Body>*/}
+        {/*  </Left>*/}
+        {/*</CardItem>*/}
+        {/*<CardItem bordered>*/}
+        {/*  <Body>*/}
+        {/*    <Text style={styles.FootNoteGray}>参考答案 提交后可见</Text>*/}
+        {/*    <Text style={styles.Body}>{this.state.answer}</Text>*/}
+        {/*  </Body>*/}
+        {/*</CardItem>*/}
         <CardItem style={{justifyContent: 'space-between'}}>
           <Label>
             <Text>最高分：</Text>
@@ -95,17 +95,17 @@ export class HomeworkInfo extends React.Component {
             <Text style={{color: 'red'}}>{this.state.minScore}</Text>
           </Label>
         </CardItem>
-        <CardItem>
-          <Body>
-            <Text style={styles.SmallHeader}>{this.state.teacher}</Text>
-            <Text style={styles.Body}>{this.state.comment}</Text>
-          </Body>
-          <Right>
-            <Button bordered small info>
-              <Text style={styles.FootNoteBlue}>整体点评</Text>
-            </Button>
-          </Right>
-        </CardItem>
+        {/*<CardItem>*/}
+        {/*  <Body>*/}
+        {/*    <Text style={styles.SmallHeader}>{this.state.teacher}</Text>*/}
+        {/*    <Text style={styles.Body}>{this.state.comment}</Text>*/}
+        {/*  </Body>*/}
+        {/*  <Right>*/}
+        {/*    <Button bordered small info>*/}
+        {/*      <Text style={styles.FootNoteBlue}>整体点评</Text>*/}
+        {/*    </Button>*/}
+        {/*  </Right>*/}
+        {/*</CardItem>*/}
       </Card>
     );
   }
